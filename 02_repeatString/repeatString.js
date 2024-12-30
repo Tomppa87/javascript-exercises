1dd6cdd8a720de05b6c525972abbdc28d283d3ea
Thomas Byrne <byrnesy987@gmail.com>
2024-12-30 19:38:47 +0200

    for loop with concat method added exception for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    result = "";
+    if (num >=0) {
+        for (i = 0; i<num; i++) {
+            result = result.concat(string)
+        }  
+    } 
+    else if (num <0) {
+        return "ERROR";
+    
+    }
+    return result
 };
 
 // Do not edit below this line
