bdd36514833d6268f151f75cdab8546b6315293d
Thomas Byrne <byrnesy987@gmail.com>
2024-12-30 22:50:07 +0200

    created array with split, reverse and join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const { reverse } = require("node:dns/promises");
 
+const reverseString = function(string) {
+    toArray = string.split("");
+    console.log(toArray);
+    toArray.reverse();
+    toArray = toArray.join("")
+    console.log(toArray);
+    return toArray
+    
 };
-
+reverseString("Hello")
 // Do not edit below this line
 module.exports = reverseString;
