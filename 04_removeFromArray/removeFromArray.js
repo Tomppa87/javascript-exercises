2b46501290bd7f324f7fb4a1271351b96a5e0f7a
Thomas Byrne <byrnesy987@gmail.com>
2024-12-31 00:06:46 +0200

    for in loop with array item check first test condition passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, num1,) {
+    
+    let newArray = [];
+    
+    for (item in array) {
+        //console.log(array[item])
+        if (array[item] != num1){
+            //console.log("not the same" +item)
+            newArray.push(array[item])
+            
+        } 
+        else continue
+        
+    }
+    console.log(newArray)
+    //console.log(array)
+    return newArray
 
+};
+removeFromArray([1,2,3,4],3)
 // Do not edit below this line
 module.exports = removeFromArray;
