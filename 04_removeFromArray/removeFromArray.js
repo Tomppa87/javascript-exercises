4e4eb99ab8b508d514bc6af5a3e66bb787cc1a21
Thomas Byrne <byrnesy987@gmail.com>
2025-01-01 19:24:45 +0200

    added spread syntax for arguments. Filter return

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,13 @@
-const removeFromArray = function(array, num1,) {
-    
-    let newArray = [];
-    
-    for (item in array) {
-        //console.log(array[item])
-        if (array[item] != num1){
-            //console.log("not the same" +item)
-            newArray.push(array[item])
-            
-        } 
-        else continue
-        
-    }
-    console.log(newArray)
-    //console.log(array)
-    return newArray
+const removeFromArray = function(array,...args) {
+    let funcArray = array;
+    console.log(funcArray)
+    console.log(args)
+    let result = array.filter(item => !args.includes(item));
+    console.log(result)        
+    return result;
 
 };
-removeFromArray([1,2,3,4],3)
+
+removeFromArray([1,2,3,4],3,2)
 // Do not edit below this line
 module.exports = removeFromArray;
