32118f1acc18c8418eb6bfbd982ca38eae5296d1
Thomas Byrne <byrnesy987@gmail.com>
2025-01-02 14:49:08 +0200

    inline if statements check if given year / exceptions are integers

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (Number.isInteger(year/4) === true) {
+        if (Number.isInteger(year/400)===true) {
+            return true
+        } else if (Number.isInteger(year/100)===true) {
+        return false
+        }
+        else return true
+    }
+     else {
+        return false
+    }
+
 
 };
+
+console.log(leapYears(1600))
 
 // Do not edit below this line
 module.exports = leapYears;
