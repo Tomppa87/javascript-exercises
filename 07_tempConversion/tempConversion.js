c60f99337c9c87d3c8ad517bb4bc36aec0766726
Thomas Byrne <byrnesy987@gmail.com>
2025-01-02 15:10:31 +0200

    add function formulas, fixed decimals, parseFloat

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+//farenheit to celcius
+const convertToCelsius = function(inputFahrenheit) {
+  let resultC = 0
+  resultC =((inputFahrenheit-32)*5/9)
+  return parseFloat(resultC.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+//celsius to farenheit
+const convertToFahrenheit = function(inputCelsius) {
+  let resultF = 0;
+  resultF = ((inputCelsius*9/5+32))
+  return parseFloat(resultF.toFixed(1))
 };
+
+console.log(convertToCelsius(100))
+console.log(convertToFahrenheit(73.2))
 
 // Do not edit below this line
 module.exports = {
