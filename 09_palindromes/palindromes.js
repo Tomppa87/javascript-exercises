a30f3c2a0951d82d64b830e334ff6109002becda
Thomas Byrne <byrnesy987@gmail.com>
2025-01-09 23:26:34 +0200

    regex for string clean, to lowercase array split, reverse and rejoin check for equality between cleaned input and reversed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const cleanedString = input.replace(/[^a-zA-Z0-9]/g, '')    
+    var lowercase = cleanedString.toLowerCase()
+    //console.log(lowercase)
+    var toArray = lowercase.split("");
+    
+    //console.log(toArray)
+    var toReverse = toArray.reverse()
+    //console.log(toReverse)
+    var joined = toReverse.join("")
+    console.log(joined);
+    
+    return joined === lowercase;
 };
+
+console.log(palindromes("Rac3e3car!!"))
 
 // Do not edit below this line
 module.exports = palindromes;
