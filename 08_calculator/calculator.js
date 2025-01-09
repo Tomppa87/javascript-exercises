ebf4e0e51357428a920676818d24aa052b5a6dde
Thomas Byrne <byrnesy987@gmail.com>
2025-01-09 23:00:14 +0200

    added functions for all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumFunc = arr.reduce((total,currentValue) => {
+    return total+currentValue;
+  },0)
+  return sumFunc
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let multipyFunction = arr.reduce((total,currentValue) => {
+    return total*currentValue;
+  },1);
+  return multipyFunction;
+};
+//console.log(multiply([1,5,6]))
 
+const power = function(num1, power) {
+  var result = 1
+	for (i = 0; i < power; i++) {
+    result *= num1;
+    console.log(result)      
+    
+  }
+  return result
+};
+//console.log(power(4,3))
+
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  else {
+  var result = num
+  console.log(num)
+  for (let i = 1; i < num; i++) {
+    result *= (num-i);
+    console.log(result)
+    
+  }
+  return result;
+  }
+  
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial(5))
 
 // Do not edit below this line
 module.exports = {
