1cfe77c0868d0be574de865b52ad907462dfa439
Thomas Byrne <byrnesy987@gmail.com>
2025-01-10 16:14:05 +0200

    attempted nested for loops

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function(array) {
+    let titles = [];
+    console.log(array)
+    for (items in array) {
+        for (key in items) {
+            console.log(key)
+        }
+        
+    }
+    return titles
 };
+
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
