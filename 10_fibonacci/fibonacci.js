847e33f2f2c5f3e3db5a63347d21b9a1b40acdb9
Thomas Byrne <byrnesy987@gmail.com>
2025-01-10 15:58:21 +0200

    for loop with 3 variables holding sequence error exception for negatives

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    input = Number(num)
+    if (input <0) {
+        return "OOPS";
+    }
+    let sequence;
+    let previous = 0;
+    let current = 1;
+    for (let i = 0; i < (input); i++) {
+        sequence = previous + current;
+        previous = current;
+        current = sequence;
+                
+    }
+    return previous
+    
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
